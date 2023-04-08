a56a9842f1e1b9c3baee253bd4f4c04e3c346f17
longieee <long.nguyen-duy@outlook.com>
2023-04-08 16:32:07 +0700

    add security + input/output

--- main.tf
+++ main.tf
@@ -13,10 +13,40 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-062550af7b9fa7d05"
-  instance_type = "t2.micro"
+  ami                    = "ami-062550af7b9fa7d05"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instace.id]
 
+  user_data = <<-EOF
+              #!/bin/bash 
+              echo "Hello, World" > index.html 
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
   tags = {
     Name = "my-terraform-example"
   }
+}
+
+resource "aws_security_group" "instace" {
+  name = "my-terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  description = "The public IP address of the web server"
+  value       = aws_instance.app_server.public_ip
 }
