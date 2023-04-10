baf8484999e35d2b462625db48f1bdafc25fc4c4
longieee <long.nguyen-duy@outlook.com>
2023-04-11 01:08:17 +0700

    add string directive conditional

--- chap-5/live/global/iam-for-each/outputs.tf
+++ chap-5/live/global/iam-for-each/outputs.tf
@@ -26,3 +26,11 @@ output "for_directive" {
 output "for_directive_index" {
   value = "%{for i, name in var.user_names}(${i}) ${name}, %{endfor}"
 }
+
+output "for_directive_index_if" {
+  value = <<EOF
+%{~for i, name in var.user_names~}
+${name}%{if i < length(var.user_names) - 1}, %{else}.%{endif}
+%{~endfor~}
+EOF
+}
