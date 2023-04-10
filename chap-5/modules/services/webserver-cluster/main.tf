9e7c7b5b9d55039d3eeb4228094ce9e3cad07e04
longieee <long.nguyen-duy@outlook.com>
2023-04-10 23:43:58 +0700

    move autoscale schedule to module

--- chap-5/modules/services/webserver-cluster/main.tf
+++ chap-5/modules/services/webserver-cluster/main.tf
@@ -121,6 +121,33 @@ resource "aws_autoscaling_group" "example" {
 }
 
 /* -------------------------------------------------------------------------- */
+/*                            Autoscaling schedules                           */
+/* -------------------------------------------------------------------------- */
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "${var.cluster_name}-scale-out-during-business-hours"
+  min_size              = 2
+  max_size              = 5
+  desired_capacity      = 5
+  recurrence            = "0 9 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+
+  scheduled_action_name = "${var.cluster_name}-scale-in-at-night"
+  min_size              = 2
+  max_size              = 5
+  desired_capacity      = 2
+  recurrence            = "0 17 * * *"
+
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+/* -------------------------------------------------------------------------- */
 /*                                Load balancer                               */
 /* -------------------------------------------------------------------------- */
 resource "aws_lb" "example" {
