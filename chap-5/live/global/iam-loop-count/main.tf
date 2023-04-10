d050126092efa558069dc27369bd367b90ad9afb
longieee <long.nguyen-duy@outlook.com>
2023-04-11 00:57:07 +0700

    add if-else count param

--- chap-5/live/global/iam-loop-count/main.tf
+++ chap-5/live/global/iam-loop-count/main.tf
@@ -1,10 +1,53 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
+/* -------------------------------------------------------------------------- */
+/*                                    Data                                    */
+/* -------------------------------------------------------------------------- */
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:Describe", "cloudwatch:Get", "cloudwatch:List"]
+    resources = ["*"]
+  }
+}
 
-module "users" {
-  source = "../../../modules/landing-zone/iam-user"
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:"]
+    resources = ["*"]
+  }
+}
 
-  count     = length(var.user_names)
-  user_name = var.user_names[count.index]
+/* -------------------------------------------------------------------------- */
+/*                              IAM Users & Roles                             */
+/* -------------------------------------------------------------------------- */
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+resource "aws_iam_policy" "cloudwatch_read_only" {
+  name   = "cloudwatch-read-only"
+  policy = data.aws_iam_policy_document.cloudwatch_read_only.json
+}
+
+resource "aws_iam_policy" "cloudwatch_full_access" {
+  name   = "cloudwatch-full-access"
+  policy = data.aws_iam_policy_document.cloudwatch_full_access.json
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+  count = var.give_neo_cloudwatch_full_access ? 1 : 0
+
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_full_access.arn
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count = var.give_neo_cloudwatch_full_access ? 0 : 1
+
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_read_only.arn
 }
