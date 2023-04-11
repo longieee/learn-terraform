1a5d5a141f37e3a75fdb7638df79c21bd04056f1
longieee <long.nguyen-duy@outlook.com>
2023-04-12 01:52:14 +0700

    add simple ASG example

---
+++ chap-8/examples/asg/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                    Data                                    */
+/* -------------------------------------------------------------------------- */
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
+/* -------------------------------------------------------------------------- */
+/*                                   Modules                                  */
+/* -------------------------------------------------------------------------- */
+module "asg" {
+  source = "../../modules/cluster/asg-rolling-deploy"
+
+  cluster_name  = var.cluster_name
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  min_size           = 1
+  max_size           = 1
+  enable_autoscaling = true
+
+  subnet_ids = data.aws_subnets.default.ids
+}
