ecf38fc0f13eb445209bdfb080e3d3fcb423b9da
longieee <long.nguyen-duy@outlook.com>
2023-04-12 01:51:42 +0700

    add live folders from ch5 (unchanged)

---
+++ chap-8/live/prod/services/webserver-clusters/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "webserver_cluster" {
+  source = "../../../../modules/services/webserver-cluster"
+
+  server_text = "Hello again, New Production with Zero_Downtime!"
+
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "dlongng-terraform-example-up-and-running-state"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 5
+  asg_custom_tags = {
+    Owner     = "dlongng"
+    ManagedBy = "terraform"
+  }
+
+  enable_autoscaling = true
+}
+
+
---
+++ chap-8/live/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "webserver_cluster" {
+  source = "../../../../modules/services/webserver-cluster"
+
+  server_text = "Hello from staging environment!"
+
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "dlongng-terraform-example-up-and-running-state"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 1
+  max_size      = 2
+
+  asg_custom_tags = {
+    Owner     = "dlongng"
+    ManagedBy = "terraform"
+  }
+
+  enable_autoscaling = false
+}
+
+# Expose extra port for testing
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
